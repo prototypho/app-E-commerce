6c100b8464c04d35764cc0bf6156f945a27ce755
prototypho <jdejonathan.es@gmail.com>
2026-01-30 13:48:53 +0100

    logo viejo fuera

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ShoppingBag, ArrowRight } from 'lucide-react';
+import { ArrowRight } from 'lucide-react';
 
 const LandingPage: React.FC = () => {
   return (
